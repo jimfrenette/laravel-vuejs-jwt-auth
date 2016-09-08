a1b440e1b3f474c57d46146e1dc86f51bc1a04e9
U-VGN-FW290\admin <jimfrenette@yahoo.com>
2016-09-08 10:17:40 -0400

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Laravel</title>
+
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+</head>
+<body>
+    <div class="container">
+        <div id="app"></div>
+    </div>
+    <script src="/js/app.js"></script>
+</body>
+</html>
